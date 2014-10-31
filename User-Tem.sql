0400ce7750f8fa5b23b29878d59b050358e1c953
Carl Morton <carlcmorton@googlemail.com>
2014-10-31 00:47:03 +0000

    Radius Wifi Project
    Contains configs, sql information

---
+++ User-Tem.sql
@@ -0,0 +1,72 @@
+INSERT INTO  `radius`.`radcheck` (
+`id` ,
+`username` ,
+`attribute` ,
+`op` ,
+`value`
+)
+VALUES (
+NULL ,  'kemoreen.freeman@excellgroup.com',  'User-Password',  ':=',  'kem1234!'
+
+INSERT INTO  `radius`.`radcheck` (
+`id` ,
+`username` ,
+`attribute` ,
+`op` ,
+`value`
+)
+VALUES (
+NULL ,  'kemoreen.freeman@excellgroup.com',  'Calling-Station-Id',  '=~',  '.*'
+
+INSERT INTO  `radius`.`radcheck` (
+`id` ,
+`username` ,
+`attribute` ,
+`op` ,
+`value`
+)
+VALUES (
+NULL ,  'kemoreen.freeman@excellgroup.com',  'Called-Station-Id',  '=~',  '.*'
+
+INSERT INTO  `radius`.`radcheck` (
+`id` ,
+`username` ,
+`attribute` ,
+`op` ,
+`value`
+)
+VALUES (
+NULL ,  'kemoreen.freeman@excellgroup.com',  'NAS-Identifier',  '=~',  '.*'
+
+INSERT INTO  `radius`.`radreply` (
+`id` ,
+`username` ,
+`attribute` ,
+`op` ,
+`value`
+)
+VALUES (
+NULL ,  'kemoreen.freeman@excellgroup.com',  'Tunnel-Private-Group-Id',  ':=',  '550'
+);
+
+INSERT INTO  `radius`.`radreply` (
+`id` ,
+`username` ,
+`attribute` ,
+`op` ,
+`value`
+)
+VALUES (
+NULL ,  'kemoreen.freeman@excellgroup.com',  'Tunnel-Medium-Type',  ':=',  'IP'
+);
+
+INSERT INTO  `radius`.`radreply` (
+`id` ,
+`username` ,
+`attribute` ,
+`op` ,
+`value`
+)
+VALUES (
+NULL ,  'kemoreen.freeman@excellgroup.com',  'Tunnel-Type',  ':=',  'GRE'
+);
